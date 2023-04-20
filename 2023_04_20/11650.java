6916548a6e4f84087962789dedfb0c4626a78d32
Minha Ahn <dks4857@gmail.com>
2023-04-20 17:41:46 +0900

    2023_04_20

---
+++ 2023_04_20/11650.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        /*for(int i = 0; i < n-1; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(arr[i][0] == arr[j][0]) {
+                    if(arr[i][1] > arr[j][1]) {
+                        int y = arr[i][1];
+                        arr[i][1] = arr[j][1];
+                        arr[j][1] = y;
+                    }
+                } else if (arr[i][0] > arr[j][0]) {
+                    int x = arr[i][0];
+                    int y = arr[i][1];
+                    arr[i][0] = arr[j][0];
+                    arr[i][1] = arr[j][1];
+                    arr[j][0] = x;
+                    arr[j][1] = y;
+                }
+            }
+        }*/
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0] == o2[0]) {
+                    return o1[1] - o2[1];
+                } else {
+                    return o1[0] - o2[0];
+                }
+            }
+        });
+
+        for(int i = 0; i < n; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+
+        br.close();
+    }
+}
