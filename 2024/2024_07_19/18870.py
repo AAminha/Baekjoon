0351372a271c4e32040c5f039271b760b1d50674
aaminha <dks4857@gmail.com>
2024-07-19 13:41:00 +0900

    2024_07_19

---
+++ 2024/2024_07_19/18870.py
@@ -0,0 +1,12 @@
+import sys
+
+N = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
+set_arr = sorted(list(set(arr)))
+
+dic = {}
+for i in range(len(set_arr)):
+  dic[set_arr[i]] = i
+
+for i in arr:
+  print(dic[i], end = " ")
