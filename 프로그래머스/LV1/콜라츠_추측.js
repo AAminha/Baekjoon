186fc0eedc03ae5243cc9d21ce1f66ac2d146c58
aaminha <dks4857@gmail.com>
2024-08-28 14:30:43 +0900

    2024.08.28 / 프로그래머스 LV1 & LV2 / JS

---
+++ 프로그래머스/LV1/콜라츠_추측.js
@@ -0,0 +1,16 @@
+function solution(num) {
+  let count = 0;
+
+  while (num > 1 && count < 500) {
+    count++;
+
+    if (num % 2 == 0) {
+      num /= 2;
+    } else {
+      num = num * 3 + 1;
+    }
+  }
+
+  if (count == 500) return -1;
+  return count;
+}
